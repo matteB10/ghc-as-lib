25ad9d227d274a5d3bef3fa347ffdb934ee52ba2
matilda <blomquistmatilda@hotmail.com>
2023-04-07 04:28:01 +0200

    cleaned up testing

--- ghc-as-lib/Test.hs
+++ ghc-as-lib/Test.hs
@@ -11,12 +11,12 @@ import GHC.Utils.Outputable
 import Compile
 import Transform
 import Similar
-import Utils 
+import Utils
 import HoleMatches
 import PrettyPrint
 
-import System.Directory (listDirectory)
-import System.FilePath ((</>), takeDirectory)
+import System.Directory (listDirectory, doesDirectoryExist)
+import System.FilePath ((</>), takeDirectory, takeExtension)
 import Data.List (isSuffixOf, isPrefixOf)
 import Data.Char (isDigit)
 import Control.Lens (rewrite)
@@ -33,49 +33,55 @@ test :: Mode -> ExerciseName -> IO ()
 -- takes the name of the exercise as a string 
 -- requires that folder with exercise name exist, with subfolders good, bad and norm. 
 test m n = do
-  let tf = case m of 
+  let tf = case m of
           DEBUG -> testPr
-          RES   -> testPrA  
+          RES   -> testPrA
   succTests n >>= \f -> banner "Expected succesful match: "
-              >> tf n f 
+              >> tf n f
   failTests n >>= \f -> banner "Expected failure: "
-              >> tf n f 
+              >> tf n f
   normaliseTests n >>= \f -> banner "Expected match with normalisation: "
-              >> tf n f 
+              >> tf n f
 
-testTc :: ExerciseName -> IO () 
-testTc fn = do 
-    files <- getAllFiles fn 
-    compiled <- mapM compFloat files 
+testTc :: ExerciseName -> IO ()
+testTc fn = do
+    files <- getExFiles fn 
+    compiled <- mapM compNorm files
+    mapM_ tcCore compiled
+
+testTcAll :: IO () 
+testTcAll = do 
+    files <- getFilePaths path 
+    compiled <- mapM compNorm files 
     mapM_ tcCore compiled 
 
-tcCore :: (CoreProgram, HscEnv) -> IO () 
+tcCore :: (CoreProgram, HscEnv) -> IO ()
 tcCore = uncurry typeCheckCore
 
-testPr :: ExerciseName -> [(FilePath,FilePath)] -> IO () 
+testPr :: ExerciseName -> [(FilePath,FilePath)] -> IO ()
 -- | Test and print test by test 
-testPr _ [] = return () 
-testPr n (t:ts) = testPr' n t >> testPr n ts  
-  where testPr' n ps@(p1,p2) = do 
-          putStrLn $ "Comparing programs " ++ show p1 `sp` show p2 
+testPr _ [] = return ()
+testPr n (t:ts) = testPr' n t >> testPr n ts
+  where testPr' n ps@(p1,p2) = do
+          putStrLn $ "Comparing programs " ++ show p1 `sp` show p2
           putStrLn "Desugar:"
-          putStr "programs match: " >> compare_desugar ps >>= print 
+          putStr "programs match: " >> compare_desugar ps >>= print
           putStrLn  "Simplifier:"
-          putStr "programs match: " >> compare_simpl ps >>= print 
+          putStr "programs match: " >> compare_simpl ps >>= print
           putStrLn  "Manual transformations:"
-          putStr "programs match: " >> compare_norm ps >>= print  
+          putStr "programs match: " >> compare_norm ps >>= print
 
-testPrA :: ExerciseName -> [(FilePath,FilePath)] -> IO () 
+testPrA :: ExerciseName -> [(FilePath,FilePath)] -> IO ()
 -- | Test and print all  
-testPrA n ps = do 
+testPrA n ps = do
   putStrLn "Desugar:"
-  putStr "programs match" >> mapM compare_desugar ps >>= print 
+  putStr "programs match" >> mapM compare_desugar ps >>= print
   putStrLn  "Simplifier:"
-  putStr "programs match" >> mapM compare_simpl ps >>= print 
+  putStr "programs match" >> mapM compare_simpl ps >>= print
   putStrLn  "Manual transformations:"
-  putStr "programs match" >> mapM compare_norm ps >>= print 
+  putStr "programs match" >> mapM compare_norm ps >>= print
   putStrLn "Float transformations"
-  putStr "programs match" >> mapM compare_float ps >>= print 
+  putStr "programs match" >> mapM compare_float ps >>= print
 
 
 comparePrint :: (FilePath -> IO CoreProgram) -> FilePath -> FilePath -> IO ()
@@ -98,7 +104,7 @@ comparePrint compile fp1 fp2 = do
   print cp2
   putStrLn $ "Programs match: " ++ show (cp1 ~== cp2)
 
-compare_ :: (FilePath -> IO CoreProgram) -> FilePath -> FilePath -> IO Bool 
+compare_ :: (FilePath -> IO CoreProgram) -> FilePath -> FilePath -> IO Bool
 compare_ comp_pass fp1 fp2 = do
   cp1' <- comp_pass fp1
   let cp1 = removeModInfo cp1'
@@ -107,17 +113,17 @@ compare_ comp_pass fp1 fp2 = do
   return (cp1 ~== cp2)
 
 
-compare_desugar, compare_simpl, compare_norm, compare_float :: (FilePath,FilePath) -> IO Bool 
-compare_desugar = uncurry $ compare_ compC 
-compare_simpl   = uncurry $ compare_ compS 
+compare_desugar, compare_simpl, compare_norm, compare_float :: (FilePath,FilePath) -> IO Bool
+compare_desugar = uncurry $ compare_ compC
+compare_simpl   = uncurry $ compare_ compS
 compare_norm    = uncurry $ compare_ compN
 compare_float   = uncurry $ compare_ compF
 
 matchSuffixedFiles :: FilePath -> IO [(FilePath, FilePath)]
 matchSuffixedFiles folderPath = do
   files <- listDirectory folderPath
-  let testFiles = filter (\f -> "Test" `isPrefixOf` f) files 
-  let modFiles = filter (\f -> "Mod" `isPrefixOf` f) files 
+  let testFiles = filter (\f -> "Test" `isPrefixOf` f) files
+  let modFiles = filter (\f -> "Mod" `isPrefixOf` f) files
   let matchingTuples = [(dir </> f1, dir </> f2) | f1 <- modFiles, f2 <- testFiles, extractSuffix f1 == extractSuffix f2]
   return matchingTuples
   where
@@ -129,25 +135,39 @@ extractSuffix filePath =
   let reversedFile = reverse filePath
       reversedSuffix = takeWhile (/='.') reversedFile ++ "."
       revSuffWNum = takeWhile isDigit (drop (length reversedSuffix) reversedFile)
-  in reverse (reversedSuffix ++ revSuffWNum) 
+  in reverse (reversedSuffix ++ revSuffWNum)
 
-getAllFiles :: ExerciseName -> IO [FilePath]
-getAllFiles n = do 
-  good <- listDirectory (n ++ "/good")
-  norm <- listDirectory (n ++ "/norm")
-  bad <- listDirectory (n ++ "/bad")
-  let good' = map (\f -> "./"++n++"/good/"++f) good 
-      bad'  = map (\f -> "./"++n++"/bad/"++f) bad 
-      norm' = map (\f -> "./"++n++"/norm/"++f) norm 
-  return $ good' ++ bad' ++ norm' 
+
+getFilePaths :: FilePath -> IO [FilePath]
+getFilePaths folderPath = do
+  isDir <- doesDirectoryExist folderPath
+  if isDir
+    then do
+      dircontent <- listDirectory folderPath
+      subFiles <- mapM getFilePaths [folderPath </> f | f <- dircontent]
+      return  $ filter isFile $ map (folderPath </>) dircontent ++ concat subFiles
+    else
+      return []
+
+isFile :: FilePath -> Bool
+isFile path = not (null (takeExtension path))
+
+
+getAllFiles = getFilePaths path
+
+path = "./testfiles/"
+
+getExFiles :: ExerciseName -> IO [FilePath]
+getExFiles n = do
+  getFilePaths $ path ++ n 
 
 succTests :: ExerciseName -> IO [(FilePath, FilePath)]
-succTests ename = matchSuffixedFiles ("./"++ename++"/good/") 
+succTests ename = matchSuffixedFiles (path++ename++"/good/")
 
 failTests :: ExerciseName -> IO [(FilePath, FilePath)]
-failTests ename = matchSuffixedFiles ("./"++ename++"/bad/") 
+failTests ename = matchSuffixedFiles (path++ename++"/bad/")
 
 normaliseTests :: ExerciseName -> IO [(FilePath, FilePath)]
-normaliseTests ename = matchSuffixedFiles ("./"++ename++"/norm/") 
+normaliseTests ename = matchSuffixedFiles (path++ename++"/norm/")
 
 
